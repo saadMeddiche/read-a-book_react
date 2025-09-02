0aa30b1fdc398849434ba21229342e3d47687dc7
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-02 11:27:16 +0100

    fix: interact with VirtualDOM instead of directly contacting DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import './App.css'
+import {useEffect, useRef, useState} from "react";
+import * as React from "react";
 
 function App() {
 
-  let degree = 0;
+  const [degreeState, setDegreeState] = useState<number>(0);
+  const intervalReference  = useRef(-1);
 
-  setInterval(() => {
-    document.getElementById('app-container')?.style.setProperty('--app-linear-gradient-degree', `${degree}deg`);
-    degree++;
-  }, 100);
+  useEffect(() => {
+
+    intervalReference.current = setInterval (() => {
+        setDegreeState((degree) => degree + 1);
+    },50)
+
+
+    return () => {
+      if(intervalReference.current) {
+        clearInterval(intervalReference.current);
+      }
+    };
+
+  });
 
   return (
     <>
-      <div className="app-container" id="app-container"></div>
+      <div className="app-container" id="app-container"
+           style={{'--app-linear-gradient-degree': `${degreeState}deg`} as React.CSSProperties}
+      >
+
+      </div>
     </>
   )
 }
