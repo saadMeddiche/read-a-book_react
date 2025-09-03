0296ad467ec934fc65f933a822bea8435e09c2a5
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-03 09:52:16 +0100

    refactor: integrate Book, Chapter, Page, and Paragraph components for structured content display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 import * as React from "react";
 import {useEffect, useRef, useState} from "react";
+import Book from "./components/book/Book.tsx";
+import Chapter from "./components/chapter/Chapter.tsx";
+import Paragraph from "./components/paragraph/Paragraph.tsx";
+import Page from "./components/page/Page.tsx";
 
 // function App() {
 //     console.log('[APP] Hello');
@@ -61,6 +65,55 @@ function App() {
             <div className="app-container" id="app-container"
                  style={{'--app-linear-gradient-degree': `${degreeState}deg`} as React.CSSProperties}
             >
+
+                <Book title={'The Galaxy'} >
+
+                    <Chapter title={'The earth'}>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                    </Chapter>
+
+                    <Chapter title={'The sun'}>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                    </Chapter>
+
+                    <Chapter title={'The moon'}>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                        <Page>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                            <Paragraph content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestias nostrum praesentium quod ratione rerum sapiente, vitae voluptatibus. Adipisci, quaerat!'}/>
+                        </Page>
+                    </Chapter>
+
+                </Book>
 
             </div>
         </>
