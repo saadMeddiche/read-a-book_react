16bedae5436e520af89b593db1c42da57857cdba
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-22 15:33:56 +0100

    feat: add flip and open states to Book

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,65 +1,81 @@
-import styles from './Book.module.css'
-import {useState} from "react";
-import Chapter, {type ChapterProps} from "../chapter/Chapter.tsx";
+import {type ChapterProps} from "../chapter/Chapter.tsx";
+import BookFrontCover from "../bookCover/front/BookFrontCover.tsx";
+import BookBackCover from "../bookCover/back/BookBackCover.tsx";
+import {BookContent} from "../bookContent/BookContent.tsx";
+import {useEffect, useState} from "react";
 
 export type BookProps = {
     id: number;
     title: string;
     author: string;
     summary: string;
+    coverImage: string;
     chapters?: ChapterProps[];
 }
 
 export default function Book(props: BookProps) {
 
-    const [pageNumber, setPageNumber] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-    const previousPage = () => {
-        if (pageNumber > 0) {
-            setPageNumber(pageNumber - 1);
-        }
-    }
+    useEffect(() => {
 
-    const nextPage = () => {
-        if (props.chapters && pageNumber < props.chapters?.length - 1) {
-            setPageNumber(pageNumber + 1);
+        console.log("Hello useEffect(flipBook)")
+
+        const flipBook = (event: KeyboardEvent) => {
+            if(event.key === " ") {
+                alert("Flipped book");
+                setIsFlipped(!isFlipped);
+            }
         }
-    }
+
+        window.addEventListener("keyup", flipBook);
+
+        return () => {
+            console.log("Bye useEffect(flipBook)")
+            window.removeEventListener("keyup", flipBook);
+        };
+
+    }, [isFlipped]);
+
+    useEffect(() => {
+
+        console.log("Hello useEffect(openOrCloseBook)")
+
+        const openOrCloseBook = (event: KeyboardEvent) => {
+            if(event.key === "Enter") {
+                alert("Open Book");
+                setIsOpen(!isOpen);
+            }
+        }
+
+        window.addEventListener("keyup", openOrCloseBook);
+
+        return () => {
+            console.log("Bye useEffect(openOrCloseBook)")
+            window.removeEventListener("keyup", openOrCloseBook);
+        };
+
+    }, [isOpen])
 
     return (
-        <div className={styles.book}>
+        <>
 
-            <div className={styles.title}>
-                <p>{props.title}</p>
-            </div>
+            {
+                !isOpen && !isFlipped && <BookFrontCover title={props.title} coverImage={props.coverImage} author={props.author} />
+            }
 
-            <div className={styles.content}>
-                {
-                        props.chapters && props.chapters.length === 0 ?
-                        ('No content')
-                        :
-                        (props.chapters && createChapter(props.chapters[pageNumber]))
-                }
-            </div>
 
-            <div className={styles.pagination}>
-                <div className={styles.leftArrow} onClick={previousPage}>
-                    &larr;
-                </div>
+            {
+                isOpen && <BookContent chapters={props.chapters} />
+            }
 
-                <div className={styles.pageNumber}>
-                    Page #{pageNumber}
-                </div>
+            {
+                !isOpen && isFlipped && <BookBackCover author={props.author} title={props.title} summary={props.summary} />
+            }
 
-                <div className={styles.rightArrow} onClick={nextPage}>
-                    &rarr;
-                </div>
-            </div>
-
-        </div>
+        </>
     );
+
 }
 
-function createChapter(chapter :ChapterProps) {
-    return <Chapter title={chapter.title} pages={chapter.pages} />
-}
